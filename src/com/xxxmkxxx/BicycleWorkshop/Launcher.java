5b42a1f484cf7073c49638af4466df24b918b0f0
”Max <maks88648@gmail.com>
2020-10-22 14:32:00 +0400

    Добавляю лаунчер для сборки jar

---
+++ src/com/xxxmkxxx/BicycleWorkshop/Launcher.java
@@ -0,0 +1,9 @@
+package com.xxxmkxxx.BicycleWorkshop;
+
+import com.sun.tools.javac.Main;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        Main.main(args);
+    }
+}

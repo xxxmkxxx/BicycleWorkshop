16ed870eb7488a86aa217fd58fdafe491c75b399
”Max <maks88648@gmail.com>
2020-10-22 15:16:34 +0400

    Исправил баг с неправильным определением класса Launcher

--- src/com/xxxmkxxx/BicycleWorkshop/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.xxxmkxxx.BicycleWorkshop;
-
-import com.sun.tools.javac.Main;
-
-public class Launcher {
-    public static void main(String[] args) throws Exception {
-        Main.main(args);
-    }
-}
